4e95e8422174e3349d880554217a033f997091ff
hariwr <hariwr25@gmail.com>
2024-12-29 02:10:48 +0700

    Menambahkan konfigurasi MahasiswaContainer dengan Retrofit dan JSON

---
+++ app/src/main/java/com/hariwr/praktikum12/container/MahasiswaContainer.kt
@@ -0,0 +1,30 @@
+package com.hariwr.praktikum12.container
+
+import com.hariwr.praktikum12.repository.MahasiswaRepository
+import com.hariwr.praktikum12.repository.NetworkMahasiswaRepository
+import com.hariwr.praktikum12.service.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer {
+    val kontakRepository: MahasiswaRepository
+}
+
+class MahasiswaContainer : AppContainer {
+
+    private val baseUrl = "http://10.0.2.2:8000/umyTI/" //localhost diganti ip kalo run di hp
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java)
+    }
+
+    override val kontakRepository: MahasiswaRepository by lazy {
+        NetworkMahasiswaRepository(mahasiswaService)
+    }
+}
