3bba6c2c8b3a98179fc99a15a4891c4230c77420
hariwr <hariwr25@gmail.com>
2024-12-29 02:39:42 +0700

    Menambahkan object DestinasiHome untuk mendefinisikan rute layar Home

---
+++ app/src/main/java/com/hariwr/praktikum12/ui/view/HomeView.kt
@@ -0,0 +1,8 @@
+package com.hariwr.praktikum12.ui.view
+
+import com.hariwr.praktikum12.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
