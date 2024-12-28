d08f0b0b9906b067b11bec4313c75b9bb7d188af
hariwr <hariwr25@gmail.com>
2024-12-29 02:46:42 +0700

    Menambahkan fungsi OnLoading dan OnError untuk menampilkan status loading dan error di layar Home

--- app/src/main/java/com/hariwr/praktikum12/ui/view/HomeView.kt
+++ app/src/main/java/com/hariwr/praktikum12/ui/view/HomeView.kt
@@ -1,16 +1,27 @@
 package com.hariwr.praktikum12.ui.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.hariwr.praktikum12.R
 import com.hariwr.praktikum12.navigation.CostumeTopAppBar
 import com.hariwr.praktikum12.navigation.DestinasiNavigasi
 import com.hariwr.praktikum12.ui.viewmodel.HomeViewModel
@@ -63,3 +74,30 @@ fun HomeScreen(
         )
     }
 }
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.ic_connection_error),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
+    }
+}
+
