08717856e47cafac5e8451aed06d85fd58e70fcd
hariwr <hariwr25@gmail.com>
2024-12-29 03:09:54 +0700

    Menambahkan fungsi PengelolaHalaman untuk mengatur navigasi antar layar

---
+++ app/src/main/java/com/hariwr/praktikum12/navigation/PengelolaHalaman.kt
@@ -0,0 +1,42 @@
+package com.hariwr.praktikum12.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.hariwr.praktikum12.ui.view.DestinasiEntry
+import com.hariwr.praktikum12.ui.view.DestinasiHome
+import com.hariwr.praktikum12.ui.view.EntryMhsScreen
+import com.hariwr.praktikum12.ui.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = {
+
+                }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route) {
+                    popUpTo(DestinasiHome.route) {
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
