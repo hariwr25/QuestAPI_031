51872c165af9e3f65bc538d6d50eaba4f2790e32
hariwr <hariwr25@gmail.com>
2024-12-29 02:35:03 +0700

    Menambahkan object DestinasiEntry untuk navigasi ke layar entry mahasiswa

---
+++ app/src/main/java/com/hariwr/praktikum12/ui/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.hariwr.praktikum12.ui.view
+
+import com.hariwr.praktikum12.navigation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
