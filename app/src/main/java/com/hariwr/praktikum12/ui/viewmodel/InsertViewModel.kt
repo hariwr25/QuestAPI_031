51852a2e07305fb6ec8c5422d582d639707fa1b1
hariwr <hariwr25@gmail.com>
2024-12-29 02:24:09 +0700

    Menambahkan data class InsertUiEvent untuk mengelola event UI

--- app/src/main/java/com/hariwr/praktikum12/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/hariwr/praktikum12/ui/viewmodel/InsertViewModel.kt
@@ -31,3 +31,11 @@ class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
 data class InsertUiState(
     val insertUiEvent:InsertUiEvent = InsertUiEvent()
 )
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
