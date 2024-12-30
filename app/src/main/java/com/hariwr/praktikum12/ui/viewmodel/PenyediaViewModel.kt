5fb1eca7cf66b5ee53b83319c335da786b8354e7
hariwr <hariwr25@gmail.com>
2024-12-30 14:35:02 +0700

    Update initializer pada PenyediaViewModel

--- app/src/main/java/com/hariwr/praktikum12/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/hariwr/praktikum12/ui/viewmodel/PenyediaViewModel.kt
@@ -1,16 +1,18 @@
 package com.hariwr.praktikum12.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.hariwr.praktikum12.MahasiswaApplications
-import com.hariwr.praktikum12.data.Mahasiswa
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
         initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { DetailViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { UpdateViewModel(createSavedStateHandle(),aplikasiMahasiswa().container.mahasiswaRepository) }
     }
 }
 
